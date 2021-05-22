6535beccf3dcb119a2e2cda0a5d9788d962a266c
Muhammad Umer <40881824+MUHammadUmerHayat@users.noreply.github.com>
2021-05-22 13:45:37 +0500

    adding footer

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React  from 'react';
+import './Footer.css';
+
+const Footer() =>{
+    <div className="Footer-container">
+    <h1>This is Footer Section</h1>
+    </div>
+}
+export default Footer;
